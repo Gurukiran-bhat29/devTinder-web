c065dfb8a7ce8fcaf4b987387048c1d9d1e58257
Gurukiran-bhat29 <gurukiranbhat@gmail.com>
2025-10-18 21:30:30 +0530

    Axios and CORS fix

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [emailId, setEmailId] = React.useState("ram@gmail.com");
+  const [password, setPassword] = React.useState("ram@123");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:7070/login", {
+        emailId,
+        password,
+      }, { 
+        withCredentials: true
+      });
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
+
   return (
-    <div>login</div>
-  )
-}
+    <div className="flex justify-center mt-20">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email Id:</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+              <div className="label"></div>
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password:</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+              <div className="label"></div>
+            </label>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
