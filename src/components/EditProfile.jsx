d1960c791bccda6d1727e1726cbae03dfe5f7634
Gurukiran-bhat29 <gurukiranbhat@gmail.com>
2025-10-21 18:11:42 +0530

    Built Edit profile

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constants";
+import UserCard from "./UserCard";
+
+const EditProfile = ({ user }) => {
+  const [firstName, setFirstName] = React.useState(user.firstName);
+  const [lastName, setLastName] = React.useState(user.lastName);
+  const [photoUrl, setPhotoUrl] = React.useState(user.photoUrl);
+  const [about, setAbout] = React.useState(user.about);
+  const dispatch = useDispatch();
+
+  const handleEditProfile = async () => {
+    try {
+      const res = await axios.patch(
+        BASE_URL + "/profile/edit",
+        {
+          firstName,
+          lastName,
+          photoUrl,
+          about,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(res.data));
+    } catch (error) {
+      console.error("Edit profile failed:", error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center mx-10 mt-20">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Edit Profile</h2>
+          <div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">First Name:</span>
+              </div>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+              <div className="label"></div>
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Last Name:</span>
+              </div>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+              <div className="label"></div>
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Photo Url:</span>
+              </div>
+              <input
+                type="text"
+                value={photoUrl}
+                onChange={(e) => setPhotoUrl(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+              <div className="label"></div>
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">About:</span>
+              </div>
+              <input
+                type="text"
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+              <div className="label"></div>
+            </label>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleEditProfile}>
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+      <UserCard user={{ firstName, lastName, photoUrl, about }} />
+    </div>
+  );
+};
+
+export default EditProfile;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 
 const UserCard = ({ user }) => {
-    console.log("User in UserCard:", user);
-    const { firstName, lastName, emailId, photoUrl, about } = user;
+  const { firstName, lastName, photoUrl, about } = user;
   return (
     <div className="card card-compact bg-base-100 w-96 shadow-xl">
       <figure>
-        <img
-          src={photoUrl}
-          alt="User"
-        />
+        <img src={photoUrl} alt="User" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{`${firstName} ${lastName}`}</h2>
         <p>{about}</p>
         <div className="card-actions justify-center mt-4">
-          <button className="btn btn-secondary">Interested</button>
           <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
         </div>
       </div>
     </div>
